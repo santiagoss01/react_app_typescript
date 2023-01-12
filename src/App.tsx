43fb3cc2c79fe83ae25a094b3d3aa69070ed5afb
C O L E C T I V O <colectivosextoa@iMac-de-Colectivo.local>
2023-01-12 13:47:59 +0100

    My first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import { useState, useEffect } from 'react';
+
+interface Sub{
+  nick:string,
+  subMonths:number,
+  avatar:string,
+  description?:string,
+
+}
+const INITIAL_STATE =[
+{
+  nick: 'Lolupo',
+  subMonths: 3,
+  avatar:'https://i.pravatar.cc/150?u=Lolupo',
+  description:'Lolupo es moderador '
+},
+{
+  nick: 'Lola_Montes',
+  subMonths: 7,
+  avatar:'https://i.pravatar.cc/150?u=Lola_montes',
+  
+}]
+interface AppState {
+  subs: Array<Sub>
+}
+
 
 function App() {
+  const [subs, setSubs] = useState <AppState['subs']> ([
+ 
+  ]) // <number string> antes del () lo convertirian en un tipo dinamico.
+  useEffect(()=> setSubs(INITIAL_STATE),[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mis subs</h1>
+      <ul>
+        {
+          subs.map(sub =>{
+            return(
+              <li key={sub.nick}>
+                <img src={sub.avatar} alt = {`Avatar for ${sub.nick}`}/>
+                <h4>{sub.nick}(<small>{sub.subMonths}</small>)</h4>
+                <p>{sub.description?.substring(0,100)}</p>
+          </li>
+            )
+          })
+        }
+
+      </ul>
+   
+
     </div>
   );
 }
